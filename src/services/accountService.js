e2a3186d1baae42aee15450f2619135bba6db71b
Korchat123 <korchat123@gmail.com>
2026-06-02 11:57:30 +0700

    clear

--- src/services/accountService.js
+++
@@ -1,7 +0,0 @@
-import { api } from "../utils/api";
-
-export const accountService = {
-  getProfile: () => api.get("/auth/me"),
-  updateProfile: (profile) => api.patch("/auth/me", profile),
-  changePassword: (passwords) => api.patch("/auth/change-password", passwords),
-};
